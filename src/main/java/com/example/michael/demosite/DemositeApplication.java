def78e4ce5f285ddf36a822d4d091b8316b4ca61
Michael Siepmann <misi1967@yahoo.de>
2018-02-23 19:19:18 +0100

    Initial commit

---
+++ src/main/java/com/example/michael/demosite/DemositeApplication.java
@@ -0,0 +1,12 @@
+package com.example.michael.demosite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemositeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemositeApplication.class, args);
+    }
+}
