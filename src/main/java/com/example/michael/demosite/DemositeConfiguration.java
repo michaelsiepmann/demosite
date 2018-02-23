79b0aa3e073ae45ede08c57d1ae20ad7007b8019
Michael Siepmann <misi1967@yahoo.de>
2018-02-23 20:38:11 +0100

    Removed configuration class

--- src/main/java/com/example/michael/demosite/DemositeConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.example.michael.demosite;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.example.michael")
-public class DemositeConfiguration {
-}
